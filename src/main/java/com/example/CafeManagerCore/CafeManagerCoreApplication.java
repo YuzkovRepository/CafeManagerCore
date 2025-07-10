8cacbf5b48b385acf2ca1db7a0950ae48b591314
Dmitriy <Dmitriyyuzkov23@gmail.com>
2025-07-10 14:17:39 +0300

    start

--- src/main/java/com/example/CafeManagerCore/CafeManagerCoreApplication.java
+++ src/main/java/com/example/CafeManagerCore/CafeManagerCoreApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CafeManagerCoreApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(CafeManagerCoreApplication.class, args);
 	}
-
 }
