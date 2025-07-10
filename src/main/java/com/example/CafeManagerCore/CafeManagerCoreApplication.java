0e4da2cccea3574a4a66a6e44b0a9da74e849575
Dmitriy <Dmitriyyuzkov23@gmail.com>
2025-07-10 14:14:43 +0300

    start

---
+++ src/main/java/com/example/CafeManagerCore/CafeManagerCoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.CafeManagerCore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafeManagerCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafeManagerCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CafeManagerCore/CafeManagerCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CafeManagerCore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafeManagerCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
